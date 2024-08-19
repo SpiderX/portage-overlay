cf420a2324abe7fa983170bb098ac514a2f329a7
Vladimir Pavljuchenkov <spiderx@spiderx.dp.ua>
2024-08-20 02:24:36 +0300

    fix(dev-php/amphp-phpunit-util): use new autoload.php from phpunit

--- dev-php/amphp-phpunit-util/files/autoload.php
+++ dev-php/amphp-phpunit-util/files/autoload.php
@@ -6,6 +6,6 @@ Fedora\Autoloader\Autoload::addPsr4('Amp\\PHPUnit\\', __DIR__);
 
 \Fedora\Autoloader\Dependencies::required([
 	'/usr/share/php/Amp/autoload.php',
-	'/usr/share/php/PHPUnit/vendor/autoload.php',
+	'/usr/share/php/PHPUnit/autoload.php',
 	'/usr/share/php/Revolt/autoload.php',
 ]);
