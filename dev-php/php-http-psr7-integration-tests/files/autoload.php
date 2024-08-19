051226e4df9e2683149bdb835b7757fb69400599
Vladimir Pavljuchenkov <spiderx@spiderx.dp.ua>
2024-08-20 02:27:36 +0300

    fix(dev-php/php-http-psr7-integration-tests): use new autoload.php from phpunit

--- dev-php/php-http-psr7-integration-tests/files/autoload.php
+++ dev-php/php-http-psr7-integration-tests/files/autoload.php
@@ -5,6 +5,6 @@ require_once 'Fedora/Autoloader/autoload.php';
 Fedora\Autoloader\Autoload::addPsr4('Http\\Psr7Test\\', __DIR__);
 
 \Fedora\Autoloader\Dependencies::required([
-	'/usr/share/php/PHPUnit/vendor/autoload.php',
+	'/usr/share/php/PHPUnit/autoload.php',
 	'/usr/share/php/Psr/Http/Message/autoload.php',
 ]);
