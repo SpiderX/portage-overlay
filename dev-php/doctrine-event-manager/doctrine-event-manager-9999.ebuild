e5f1cc35f63d87981b346f14c40c3bdebc067b11
Vladimir Pavljuchenkov <spiderx@spiderx.dp.ua>
2025-09-21 13:09:10 +0300

    fix(dev-php/doctrine-event-manager): drop x86 arch

--- dev-php/doctrine-event-manager/doctrine-event-manager-9999.ebuild
+++ dev-php/doctrine-event-manager/doctrine-event-manager-9999.ebuild
@@ -1,14 +1,13 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
-
-EGIT_REPO_URI="https://github.com/doctrine/event-manager.git"
 
 inherit git-r3
 
 DESCRIPTION="Doctrine Event Manager"
 HOMEPAGE="https://github.com/doctrine/event-manager"
+EGIT_REPO_URI="https://github.com/doctrine/event-manager.git"
 
 LICENSE="MIT"
 SLOT="0"
--- dev-php/doctrine-event-manager/files/autoload.php
+++ dev-php/doctrine-event-manager/files/autoload.php
@@ -3,10 +3,10 @@
 require_once 'Fedora/Autoloader'.'/autoload.php';
 
 \Fedora\Autoloader\Autoload::addClassMap(
-    array(
-        'doctrine\\common\\eventargs' => '/../EventArgs.php',
-        'doctrine\\common\\eventmanager' => '/../EventManager.php',
-        'doctrine\\common\\eventsubscriber' => '/../EventSubscriber.php',
-    ),
-    __DIR__
+	array(
+		'doctrine\\common\\eventargs' => '/../EventArgs.php',
+		'doctrine\\common\\eventmanager' => '/../EventManager.php',
+		'doctrine\\common\\eventsubscriber' => '/../EventSubscriber.php',
+	),
+	__DIR__
 );
